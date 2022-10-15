bceb473fcd532bd2ae3f19691b583859903c90a4
Chris Adams <chris.adams@ziply.com>
2022-10-15 14:33:54 -0700

    updates/bugfixes

--- main1.py
+++ main1.py
@@ -12,4 +12,21 @@ env = Environment(loader=FileSystemLoader(self.template_path),
                         autoescape=select_autoescape(['html', 'xml']))
 headers = json.loads(self.env.get_template('headers.json').render())
 
+api_key = "API-324C-1669-469F-9148"
+url = f"https://reg.arin.net"
+
+
+def test(self, as_set):
+    """
+
+    :param asn
+    :return: Tuple. Index 0 is http status code and index 1 is response dict.
+    """
+    post = requests.get(f"{url}/rest/irr/as-set/{as_set}?apikey={api_key}",
+                            headers=self.headers,
+                            )
+
+    resp = post.json()
+
+    return post.status_code, resp
 
