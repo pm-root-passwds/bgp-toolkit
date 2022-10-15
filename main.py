70e3db1974ae1a0419bec6015306e07b210281ce
Chris Adams <chris.adams@ziply.com>
2022-10-15 15:19:03 -0700

    updates/bugfixes

--- main.py
+++ main.py
@@ -12,28 +12,32 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class PyArin:
     def __init__(self):
-        self.url="https://reg.arin.net"
+        self.url = "https://reg.arin.net"
         self.template_path = templates.__path__[0]
-        self.env = Environment(loader=FileSystemLoader(self.template_path),
-                          autoescape=select_autoescape(['html', 'xml']))
-        self.headers = json.loads(self.env.get_template('headers.json').render())
+        self.env = Environment(
+            loader=FileSystemLoader(self.template_path),
+            autoescape=select_autoescape(["html", "xml"]),
+        )
+        self.headers = json.loads(self.env.get_template("headers.json").render())
 
-        self.api_key = os.environ.get('ARIN_API_KEY')
+        self.api_key = os.environ.get("ARIN_API_KEY")
+
     def by_as_set(self, as_set):
         """
 
         :param asn
         :return: Tuple. Index 0 is http status code and index 1 is response dict.
         """
-        get = requests.get(f"{self.url}/rest/irr/as-set/{as_set}?apikey={api_key}",
-                                headers=headers,
-                                )
+        get = requests.get(
+            f"{self.url}/rest/irr/as-set/{as_set}?apikey={api_key}",
+            headers=headers,
+        )
 
         doc = xmltodict.parse(get.text)
 
-
         return get.status_code, json.dumps(doc)
         # return get.status_code, resp
+
 
 test = PyArin()
 print(test.by_as_set("AS-IONSWITCH"))
