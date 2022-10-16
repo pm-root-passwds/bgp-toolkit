a29f357119a484c3964fd30f4a8c2a35aa235e6c
Nick Bogle <nick@ionswitch.com>
2022-10-15 18:22:35 -0700

    Add IRR methods

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import os
 import json
+from sys import prefix
 import xmltodict
 import requests
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import templates
 import urllib3
+import re
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
 
 class PyArin:
     def __init__(self):
@@ -19,9 +20,9 @@ class PyArin:
             autoescape=select_autoescape(["html", "xml"]),
         )
         self.headers = json.loads(self.env.get_template("headers.json").render())
-
+        self.headers_rpsl = json.loads(self.env.get_template("headers_rpsl.json").render())
         self.api_key = os.environ.get("ARIN_API_KEY")
-
+    
     def by_as_set(self, as_set):
         """
 
@@ -160,7 +161,79 @@ class PyArin:
         doc = xmltodict.parse(get.text)
         # print(doc)
         return get.status_code, json.dumps(doc)
+###IRR###
+    def create_route(self, prefix, asn, descr, admin_c, tech_c, mnt_by):
+        body = (f"""route: {prefix}
+origin: {asn}
+descr: {descr}
+admin-c: {admin_c}
+tech-c: {tech_c}
+mnt-by: {mnt_by}
+source: ARIN""")
+        req = requests.post(f'{self.url}/irr/route/{prefix}/{asn}?apikey={self.api_key}', 
+        headers=self.headers_rpsl,
+        data=body,
+        )
+        return req
 
+    def create_route6(self, prefix, asn, descr, admin_c, tech_c, mnt_by):
+        body = (f"""route6: {prefix}
+origin: {asn}
+descr: {descr}
+admin-c: {admin_c}
+tech-c: {tech_c}
+mnt-by: {mnt_by}
+source: ARIN""")
+        req = requests.post(f'{self.url}/irr/route/{prefix}/{asn}?apikey={self.api_key}', 
+        headers=self.headers_rpsl,
+        data=body,
+        )
+        return req
+    
+
+    def get_route_object(self, prefix, asn):
+        get = requests.get(f'{self.url}/irr/route/{prefix}/{asn}?apikey={self.api_key}',
+        headers=self.headers_rpsl)
+        splitter = re.compile(r'^(\S+): +(.*)$')
+        lines = get.text.split('\n')
+        results = dict()
+        for line in lines:
+            match = splitter.findall(line)
+            if match:
+                results[match[0][0]] = match[0][1]
+        return results
+
+    def modify_route_object(self, prefix, asn, descr):
+        existing = self.get_route_object(prefix, asn)
+        body = (f"""route: {prefix}
+origin: {asn}
+descr: {descr}
+admin-c: {existing['admin-c']}
+tech-c: {existing['tech-c']}
+mnt-by: {existing['mnt-by']}
+source: ARIN""")
+        put = requests.put(f'{self.url}/irr/route/{prefix}/{asn}?apikey={self.api_key}',
+        headers=self.headers_rpsl,
+        data = body)
+        return put
+
+    def modify_route6_object(self, prefix, asn, descr):
+        existing = self.get_route_object(prefix, asn)
+        body = (f"""route6: {prefix}
+origin: {asn}
+descr: {descr}
+admin-c: {existing['admin-c']}
+tech-c: {existing['tech-c']}
+mnt-by: {existing['mnt-by']}
+source: ARIN""")
+        put = requests.put(f"{self.url}/irr/route/{prefix}/{asn}?apikey={self.api_key}",
+        headers=self.headers_rpsl,
+        data = body)
+        return put
+
+    def delete_route_object(self, prefix, asn):
+        delete = requests.delete(f"{self.url}/irr/route/{prefix}/{asn}?apikey={self.api_key}", headers=headers_rpsl)
+        return delete
 
 test = PyArin()
 # print(test.get_ticket_summaries(ticket_type="ASN_REQUEST", ticket_status="CLOSED"))
