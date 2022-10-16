ee96b45e3e62581fecac8b3549d349a37bfad2bc
Christopher Holbrook <chris.holbrook@ziply.com>
2022-10-16 04:18:29 -0700

    Format strings fix

--- irr.py
+++ irr.py
@@ -1,4 +1,14 @@
-from arin import *
+#!/usr/bin/python3
+import json
+import os
+import re
+import requests
+import templates
+import urllib3
+import xmltodict
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from sys import prefix
+from arin import Arin
 
 
 class IRR(Arin):
