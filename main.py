6a82a010fb8121e7b772b329ad35ebd3cb115ab9
Nick Bogle <nick@ionswitch.com>
2022-10-15 18:28:01 -0700

    Add 3 more IRR methods

--- main.py
+++ main.py
@@ -258,6 +258,37 @@ source: ARIN""")
                 results[match[0][0]] = match[0][1]
         return results
 
+    def modify_route_object(self, prefix, asn, descr):
+        existing = self.get_route_object(prefix, asn)
+        body = (f"""route: {prefix}
+origin: {asn}
+descr: {descr}
+admin-c: {existing['admin-c']}
+tech-c: {existing['tech-c']}
+mnt-by: {existing['mnt-by']}
+source: ARIN""")
+        put = requests.put(f'{self.url}/irr/route/{prefix}/{asn}?apikey={self.api_key}',
+        headers=self.headers_rpsl,
+        data = body)
+        return put
+
+    def modify_route6_object(self, prefix, asn, descr):
+        existing = self.get_route_object(prefix, asn)
+        body = (f"""route6: {prefix}
+origin: {asn}
+descr: {descr}
+admin-c: {existing['admin-c']}
+tech-c: {existing['tech-c']}
+mnt-by: {existing['mnt-by']}
+source: ARIN""")
+        put = requests.put(f"{self.url}/irr/route/{prefix}/{asn}?apikey={self.api_key}",
+        headers=self.headers_rpsl,
+        data = body)
+        return put
+
+    def delete_route_object(self, prefix, asn):
+        delete = requests.delete(f"{self.url}/irr/route/{prefix}/{asn}?apikey={self.api_key}", headers=headers_rpsl)
+        return delete
 
 test = PyArin()
 # print(test.get_ticket_summaries(ticket_type="ASN_REQUEST", ticket_status="CLOSED"))
